fa721c602e8d30c03d019ced31cd415bfbfdc29b
Kdangelo <kdangelof@gmail.com>
2018-01-30 15:42:36 -0300

    modifying files

--- assets/libreria/test/test.js
+++
@@ -1,8 +0,0 @@
-const { assert } = require('chai');
-const imgFilter = require('../js/filtro.js');
-
-describe('cargaFiltro()', () => {
-	it('checking if sepia is equal to null', () => {
-	assert.equal('sepia == number', false);
-});
-});
--- test/test.js
+++
@@ -1,5 +0,0 @@
-const chai = require('chai');
-
-const jsdom = require("jsdom");
-const {JSDOM} = jsdom;
-let dom = new JSDOM('<!doctype html><html><body onload="cargaFiltro();"><img id')
